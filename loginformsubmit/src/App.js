6120ed94aaa966209c147a82e57c2305d291aa00
Lakshmi Sowjanya <lakshmisowjanyadonadri1@gmail.com>
2023-06-22 13:24:00 +0530

    Added second submit bar

--- loginformsubmit/src/App.js
+++ loginformsubmit/src/App.js
@@ -3,26 +3,47 @@ import React, { useState } from "react";
 const App = ()=>{
 
   const [name, setName] = useState("");
-  const[fullName, setFullName] = useState();
+  const[fullName, setFullName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [lastNameNew, setLastNameNew] = useState("");
 
+  const onSubmit = (event)=>{
+    event.preventDefault();
+    setFullName(name);
+    setLastNameNew(lastNameNew);
+  };
+  
   const inputEvent = (event)=>{
+    console.log(event.target.value)
     setName(event.target.value);
   };
-
-  const onSubmit = ()=>{
-    setFullName(name);
+  const inputEvent2 = (event)=>{
+    console.log(event.target.value)
+    setLastName(event.target.value);
   };
+
+  
 
   return(
     <>
     <div>
-      <h1> Hello {fullName} </h1>
+      <form>
+        <div>
+      <h1> Hello {fullName}{" "}{lastName} </h1>
       <input type="text" 
-              placeholder="Enter your name" 
+              placeholder="Enter your First name" 
               onChange={inputEvent}
               value={name}
               />
+      <br/>
+      <input type="text" 
+              placeholder="Enter your Last name" 
+              onChange={inputEvent2}
+              value={lastName}
+              />
       <button onClick={onSubmit}>Enter</button>
+      </div>
+      </form>
     </div>
     </>
   )
